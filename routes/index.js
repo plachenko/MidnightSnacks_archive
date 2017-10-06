2b11b19338636c7dff76bcc996ef098fa5f8d13f
Denis Perchenko <plachenkO@gmail.com>
2017-10-06 15:21:46 -0400

    formatting

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,24 @@ var url = [];
 function buildURLs(){
   var show_re = /[^=show\/]([^\/]+)/;
   request('http://midnightsnacks.fm', (err, respond, body)=>{
-    var $ = cheerio.load(body);
-  $('.dates a').map(function(i, el){
-    var link = $(this).attr('href');
-    var showNum = show_re.exec(link)[0];
-    if(showNum >= 11){
-      if(showNum > 400){
-        url.push('http://midnightsnacks.fm/archive/Midnight_Snacks-'+showNum+'_'+link.substr(-8)+'.mp3');
-      }else{
-        url.push('http://midnightsnacks.fm/archive/Midnight_Snacks-'+link.substr(-8)+'.mp3');
-      }
-    }
 
-  });
+    var $ = cheerio.load(body);
+
+    $('.dates a').map(function(i, el){
+      var link = $(this).attr('href');
+      var showNum = show_re.exec(link)[0];
+
+      //Only get shows after 11 since that's when show downloads started to happen...
+      if(showNum >= 11){
+        //After the 400th show switch naming convention to Midnight_Snacks-[showNumber]_[date].mp3
+        if(showNum > 400){
+          url.push('http://midnightsnacks.fm/archive/Midnight_Snacks-'+showNum+'_'+link.substr(-8)+'.mp3');
+        }else{
+          url.push('http://midnightsnacks.fm/archive/Midnight_Snacks-'+link.substr(-8)+'.mp3');
+        }
+      }
+
+    });
 
   });
 }
